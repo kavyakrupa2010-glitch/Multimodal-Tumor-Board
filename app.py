2911c32ed3b9a4e7098859be13e797cea6a9c669
kavyakrupa2010-glitch <kavyakrupa2010@gmail.com>
2026-05-01 15:40:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+# Streamlit Cloud SQLite3 hotfix for CrewAI/ChromaDB
+import sys
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import streamlit as st
 import os
 
